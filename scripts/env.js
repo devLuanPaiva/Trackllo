2d7bfe2713bcce1f53eac9b5627108bd25185c8a
Luan <devluanpaiva@gmail.com>
2025-03-17 16:44:52 -0300

    chore: create env script

---
+++ scripts/env.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const dotenv = require("dotenv");
+const path = require("path");
+
+dotenv.config();
+
+const environmentsPath = path.join(__dirname, "../src/environments");
+
+if (!fs.existsSync(environmentsPath)) {
+  fs.mkdirSync(environmentsPath, { recursive: true });
+}
+
+const environmentFile = (isProd) => `
+export const environment = {
+  production: ${isProd},
+  API_WRITE: '${process.env.API_WRITE || ""}',
+  PROJECT_ID: '${process.env.PROJECT_ID || ""}',
+};
+`;
+
+fs.writeFileSync(
+  path.join(environmentsPath, "environment.ts"),
+  environmentFile(true)
+);
+fs.writeFileSync(
+  path.join(environmentsPath, "environment.development.ts"),
+  environmentFile(false)
+);
