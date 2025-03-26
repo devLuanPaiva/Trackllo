b7c1be5b9ac7bcf5ef776c386926b5df700580ac
Luan <devluanpaiva@gmail.com>
2025-03-26 13:31:57 -0300

    feat: start implement home screen

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../../services/board.service';
+import { AuthenticationService } from '../../services/authentication.service';
+import { IBoard } from '../../models';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  boards$: Observable<IBoard[]> | null = null;
+  userId: string | null = null;
 
+  constructor(
+    private readonly boardService: BoardService,
+    private readonly authService: AuthenticationService
+  ) { }
+
+  ngOnInit(): void {
+    const user = this.authService.getLoggedUser();
+    if (user) {
+      this.userId = user.id;
+      this.loadBoards();
+    }
+  }
+
+  loadBoards(): void {
+    if (this.userId) {
+      this.boards$ = this.boardService.getBoards(this.userId);
+    }
+  }
+
+  createBoard(): void {
+    if (this.userId) {
+      this.boardService.createBoard(this.userId).subscribe(() => {
+        this.loadBoards();
+      });
+    }
+  }
 }
