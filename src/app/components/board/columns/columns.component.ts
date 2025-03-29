b7b511bca72db93b0bea65451db5fdb74fe335e0
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-29 20:05:28 -0300

    feat: implement task form

--- src/app/components/board/columns/columns.component.ts
+++ src/app/components/board/columns/columns.component.ts
@@ -1,27 +1,52 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ITask } from '../../../models';
 import { CdkDrag, CdkDragDrop, CdkDropList } from '@angular/cdk/drag-drop';
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import {
+  ReactiveFormsModule,
+  FormsModule,
+  FormGroup,
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-columns',
-  imports: [CommonModule, CdkDropList, CdkDrag, FontAwesomeModule],
+  imports: [
+    CommonModule,
+    CdkDropList,
+    CdkDrag,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './columns.component.html',
   animations: [
     trigger('fadeInOut', [
       state('void', style({ opacity: 0, transform: 'scale(0.95)' })),
       transition('void => *', [animate('300ms ease-in')]),
-      transition('* => void', [animate('200ms ease-out', style({ opacity: 0, transform: 'scale(0.95)' }))])
-    ])
-  ]
+      transition('* => void', [
+        animate(
+          '200ms ease-out',
+          style({ opacity: 0, transform: 'scale(0.95)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class ColumnsComponent {
   @Input() columnTitle: string = '';
   @Input() columnTasks: ITask[] = [];
-  @Input() id: string = ''; 
+  @Input() id: string = '';
   @Input() connectedTo: string[] = [];
   @Input() colorTask: string = 'blue';
   @Output() taskDropped = new EventEmitter<CdkDragDrop<ITask[]>>();
@@ -29,6 +54,36 @@ export class ColumnsComponent {
   icons = {
     plus: faPlus,
   };
+  showForm: boolean = false;
+  taskForm: FormGroup;
+
+  constructor(private readonly fb: FormBuilder) {
+    this.taskForm = this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      image: [''],
+    });
+  }
+
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
+
+  addTask() {
+    if (this.taskForm.valid) {
+      const newTask: ITask = {
+        id: crypto.randomUUID(),
+        title: this.taskForm.value.title,
+        description: this.taskForm.value.description,
+        image: this.taskForm.value.image || undefined,
+        userId: 'default-user',
+      };
+
+      this.columnTasks.push(newTask);
+      this.taskForm.reset();
+      this.showForm = false;
+    }
+  }
 
   onMoveColumnTask(event: CdkDragDrop<ITask[]>) {
     this.taskDropped.emit(event);
