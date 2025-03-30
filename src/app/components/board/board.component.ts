fc47678ceba36cba67ec18b1674a67d04648ce37
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-30 11:59:37 -0300

    feat: create loadUserBoard method

--- src/app/components/board/board.component.ts
+++ src/app/components/board/board.component.ts
@@ -1,5 +1,4 @@
 import { IBoard, ITask } from '../../models';
-import { BOARD_DATA } from './../../constants/boards.constant';
 import { Component, OnInit } from '@angular/core';
 import {
   CdkDragDrop,
@@ -7,10 +6,18 @@ import {
   transferArrayItem,
 } from '@angular/cdk/drag-drop';
 import { CommonModule } from '@angular/common';
-import { FontAwesomeModule } from "@fortawesome/angular-fontawesome"
-import { faPlus } from "@fortawesome/free-solid-svg-icons"
-import { trigger, state, style, transition, animate } from '@angular/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
 import { ColumnsComponent } from './columns/columns.component';
+import { AuthenticationService } from '../../services/authentication.service';
+import { BoardService } from '../../services/board.service';
 
 @Component({
   selector: 'app-board',
@@ -21,39 +28,63 @@ import { ColumnsComponent } from './columns/columns.component';
     trigger('fadeInOut', [
       state('void', style({ opacity: 0, transform: 'scale(0.95)' })),
       transition('void => *', [animate('300ms ease-in')]),
-      transition('* => void', [animate('200ms ease-out', style({ opacity: 0, transform: 'scale(0.95)' }))])
-    ])
-  ]
+      transition('* => void', [
+        animate(
+          '200ms ease-out',
+          style({ opacity: 0, transform: 'scale(0.95)' })
+        ),
+      ]),
+    ]),
+  ],
 })
 export class BoardComponent implements OnInit {
-  boards: IBoard = BOARD_DATA;
+  boards: IBoard = {
+    id: '',
+    userId: '',
+    columns: [],
+  };
   columnTodo: ITask[] = [];
   columnInProgress: ITask[] = [];
   columnDone: ITask[] = [];
   icons = {
     plus: faPlus,
-  }
-  constructor() { }
+  };
+  constructor(
+    private readonly authService: AuthenticationService,
+    private readonly boardService: BoardService
+  ) {}
 
   ngOnInit(): void {
-    const todoColumn = this.boards.columns.find(column => column.title === 'Todo');
-    const inProgressColumn = this.boards.columns.find(column => column.title === 'In Progress');
-    const doneColumn = this.boards.columns.find(column => column.title === 'Done');
-
-    this.columnTodo = todoColumn ? todoColumn.tasks : [];
-    this.columnInProgress = inProgressColumn ? inProgressColumn.tasks : [];
-    this.columnDone = doneColumn ? doneColumn.tasks : [];
+   this.loadUserBoard()
+  }
+  loadUserBoard(): void{
+    const userId = this.authService.getLoggedUser()?.id
+    this.boardService.getUserBoards(userId!).subscribe({
+      next: (board) => {
+        this.boards = board[0];
+        const todoColumn = board[0]?.columns.find((column) => column.title === 'To do');
+        const inProgressColumn = board[0]?.columns.find((column) => column.title === 'In Progress');
+        const doneColumn = board[0]?.columns.find((column) => column.title === 'Done');
+        this.columnTodo = todoColumn ? todoColumn.tasks : [];
+        this.columnInProgress = inProgressColumn ? inProgressColumn.tasks : [];
+        this.columnDone = doneColumn ? doneColumn.tasks : [];
+      }
+    })
   }
 
   onMoveColumnTask(event: CdkDragDrop<ITask[]>) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
     } else {
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
         event.previousIndex,
-        event.currentIndex,
+        event.currentIndex
       );
     }
   }
