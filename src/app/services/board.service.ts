3f8bcca16daac957c19038bf8f7886564e96adcc
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-03-30 10:45:21 -0300

    refactor: change all methos

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -1,84 +1,112 @@
 import { Injectable } from '@angular/core';
+import { catchError, from, map, Observable } from 'rxjs';
+import { IBoard, ITask, IColumn } from '../models';
+import { database, ID, Query } from '../../lib/appwrite';
 import { environment } from '../../environments/environment';
-import { catchError, from, map, Observable, switchMap } from 'rxjs';
-import { IBoard, ITask } from '../models';
-import { database, ID } from '../../lib/appwrite';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class BoardService {
   private readonly databaseId = environment.DATABASE_ID;
-  private readonly collectionId = environment.BOARD_COLLECTION_ID;
+  private readonly BOARD_ID = environment.BOARD_COLLECTION_ID;
+  private readonly COLUMNS_ID = environment.COLUMNS_COLLECTION_ID;
+  private readonly TASKS_ID = environment.TASKS_COLLECTION_ID;
 
-  constructor() { }
-  createBoard(userId: string, title: string): Observable<IBoard> {
-    const board = {
-      userId,
-      title,
-      columns: [
-        { id: ID.unique(), title: 'Todo', tasks: [] },
-        { id: ID.unique(), title: 'In Progress', tasks: [] },
-        { id: ID.unique(), title: 'Done', tasks: [] },
-      ]
-    };
-    const permissions = [
-      `read("user:${userId}")`,
-      `write("user:${userId}")`,
-      `update("user:${userId}")`,
-      `delete("user:${userId}")`
-    ];
+  constructor() {}
 
-    return from(database.createDocument(
-      this.databaseId,
-      this.collectionId,
-      ID.unique(),
-      board,
-      permissions
-    )).pipe(
-      map(doc => doc as unknown as IBoard),
-      catchError(error => {
+  createBoard(userId: string, columnIds: string[]): Observable<IBoard> {
+    return from(
+      database.createDocument(this.databaseId, this.BOARD_ID, ID.unique(), {
+        userId,
+        columns: columnIds,
+      })
+    ).pipe(
+      map(
+        (doc) =>
+          ({
+            id: doc.$id,
+            userId: doc['userId'],
+            columns: doc['columns'],
+          } as IBoard)
+      ),
+      catchError((error) => {
         console.error('Erro ao criar board:', error);
         throw error;
       })
     );
   }
 
-  getBoards(userId: string): Observable<IBoard[]> {
-    return from(database.listDocuments(this.databaseId, this.collectionId, [
-      `equal("userId", "${userId}")`
-    ])).pipe(
-      map(response => response.documents as unknown as IBoard[]),
-      catchError(error => {
+  createColumn(
+    title: 'Todo' | 'In Progress' | 'Done',
+    taskIds: string[]
+  ): Observable<IColumn> {
+    return from(
+      database.createDocument(this.databaseId, this.COLUMNS_ID, ID.unique(), {
+        title,
+        tasks: taskIds, 
+      })
+    ).pipe(
+      map(
+        (doc) =>
+          ({ id: doc.$id, title: doc['title'], tasks: doc['tasks'] } as IColumn)
+      ),
+      catchError((error) => {
+        console.error('Erro ao criar coluna:', error);
+        throw error;
+      })
+    );
+  }
+
+  createTask(
+    userId: string,
+    title: string,
+    description?: string,
+    imageUrl?: string
+  ): Observable<ITask> {
+    return from(
+      database.createDocument(this.databaseId, this.TASKS_ID, ID.unique(), {
+        userId,
+        title,
+        description,
+        imageUrl,
+      })
+    ).pipe(
+      map(
+        (doc) =>
+          ({
+            id: doc.$id,
+            userId: doc['userId'],
+            title: doc['title'],
+            description: doc['description'],
+            image: doc['imageUrl'],
+          } as ITask)
+      ),
+      catchError((error) => {
+        console.error('Erro ao criar tarefa:', error);
+        throw error;
+      })
+    );
+  }
+
+  getUserBoards(userId: string): Observable<IBoard[]> {
+    return from(
+      database.listDocuments(this.databaseId, this.BOARD_ID, [
+        Query.equal('userId', userId),
+      ])
+    ).pipe(
+      map((res) =>
+        res.documents.map(
+          (doc) =>
+            ({
+              id: doc.$id,
+              userId: doc['userId'],
+              columns: doc['columns'],
+            } as IBoard)
+        )
+      ),
+      catchError((error) => {
         console.error('Erro ao buscar boards:', error);
-        throw error;
-      })
-    );
-  }
-
-  addTask(boardId: string, columnId: string, task: ITask): Observable<IBoard> {
-    return this.getBoard(boardId).pipe(
-      map(board => {
-        const column = board.columns.find(col => col.id === columnId);
-        if (column) {
-          column.tasks.push({ ...task, id: ID.unique() });
-        }
-        return board;
-      }),
-      switchMap(updatedBoard => from(database.updateDocument(this.databaseId, this.collectionId, boardId, updatedBoard))),
-      map(doc => doc as unknown as IBoard),
-      catchError(error => {
-        console.error('Erro ao adicionar task:', error);
-        throw error;
-      })
-    );
-  }
-
-  getBoard(boardId: string): Observable<IBoard> {
-    return from(database.getDocument(this.databaseId, this.collectionId, boardId)).pipe(
-      map(doc => doc as unknown as IBoard),
-      catchError(error => {
-        console.error('Erro ao obter board:', error);
         throw error;
       })
     );
