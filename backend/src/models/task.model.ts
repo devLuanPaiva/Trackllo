7301364bcb163ec95e67ad560e883e7ea9795dfa
Luan <devluanpaiva@gmail.com>
2025-04-01 15:50:42 -0300

    feat: create task model

---
+++ backend/src/models/task.model.ts
@@ -0,0 +1,9 @@
+import { Prisma } from '@prisma/client'
+
+export type Task = Prisma.TaskGetPayload<{
+    include: {
+        user: true,
+        column: true
+    }
+}>;
+export type CreateTaskInput = Prisma.TaskCreateInput
