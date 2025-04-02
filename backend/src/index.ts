084081deefefab5c78b906468ad12090ebe6b177
Luan <devluanpaiva@gmail.com>
2025-04-02 11:46:53 -0300

    feat: add app routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,25 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import { cors } from 'hono/cors'
+import userRoutes from './controllers/user.controller.js'
+import taskRoutes from './controllers/task.controller.js'
+import boardRoutes from './controllers/board.controller.js'
+import errorHandler from './utils/errorHandler.js'
 
 const app = new Hono()
+app.use('*', cors())
+app.use('*', async (c, next) => {
+  console.log(`[${c.req.method}] ${c.req.url}`)
+  await next()
+})
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
+app.route('/users', userRoutes)
+app.route('/tasks', taskRoutes)
+app.route('/boards', boardRoutes)
+app.onError(errorHandler)
 
 serve({
   fetch: app.fetch,
