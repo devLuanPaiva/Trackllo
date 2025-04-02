08c6b47ceb9376fcc69365ae9a62cdfeff1cbbd9
Luan <devluanpaiva@gmail.com>
2025-04-02 13:32:53 -0300

    feat: create column module

--- backend/src/models/column.model.ts
+++ backend/src/models/column.model.ts
@@ -1,6 +1,6 @@
 import { Prisma } from '@prisma/client'
 
-export type User = Prisma.ColumnGetPayload<{
+export type Column = Prisma.ColumnGetPayload<{
     include: {
         board: true,
         tasks: true
