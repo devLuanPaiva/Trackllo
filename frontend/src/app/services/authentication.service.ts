50d51f36dce8a8203e0b00cece0f8724a327e186
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-05 10:24:06 -0300

    feat: add getAuthHeader method

--- frontend/src/app/services/authentication.service.ts
+++ frontend/src/app/services/authentication.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
 import { IUser } from '../models';
@@ -70,5 +70,11 @@ export class AuthenticationService {
 
   isAuthenticated(): boolean {
     return this.getToken() !== null;
+  }
+  getAuthHeaders(): HttpHeaders {
+    const token = this.getToken();
+    return new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
   }
 }
