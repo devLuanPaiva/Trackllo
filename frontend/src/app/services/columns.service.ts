88b06cece00d83a9c88b6b9a0f4ff93ea9b4bb72
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-05 14:45:03 -0300

    feat: create columns service

---
+++ frontend/src/app/services/columns.service.ts
@@ -0,0 +1,45 @@
+import { IColumn } from '../models';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AuthenticationService } from './authentication.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ColumnsService {
+  private readonly api_url = environment.API_URL;
+  constructor(
+    private readonly http: HttpClient,
+    private readonly authService: AuthenticationService
+  ) {}
+  getBoardColumns(boardId: string): Observable<IColumn[]> {
+    return this.http
+      .get<IColumn[]>(`${this.api_url}/columns/board/${boardId}`, {
+        headers: this.authService.getAuthHeaders(),
+      })
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching boards:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar colunas')
+          );
+        })
+      );
+  }
+  getColumnById(columnId: string): Observable<IColumn> {
+    return this.http
+      .get<IColumn>(`${this.api_url}/columns/${columnId}`, {
+        headers: this.authService.getAuthHeaders(),
+      })
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching boards:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar coluna')
+          );
+        })
+      );
+  }
+}
