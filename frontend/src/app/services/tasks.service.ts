1cc5918e353a5c0585677b76f98c183082189d39
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-05 14:45:18 -0300

    feat; create tasks service

---
+++ frontend/src/app/services/tasks.service.ts
@@ -0,0 +1,111 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { AuthenticationService } from './authentication.service';
+import { catchError, Observable, throwError } from 'rxjs';
+import { ITask } from '../models';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  private readonly api_url = environment.API_URL;
+  constructor(
+    private readonly http: HttpClient,
+    private readonly authService: AuthenticationService
+  ) {}
+
+  getColumnTasks(columnId: string): Observable<ITask[]> {
+    const params = new HttpParams().set('columnId', columnId);
+
+    return this.http
+      .get<ITask[]>(`${this.api_url}/tasks`, {
+        headers: this.authService.getAuthHeaders(),
+        params,
+      })
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching tasks:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar tasks')
+          );
+        })
+      );
+  }
+  getTaskById(id: string): Observable<ITask> {
+    return this.http
+      .get<ITask>(`${this.api_url}/tasks/${id}`, {
+        headers: this.authService.getAuthHeaders(),
+      })
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching boards:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar task')
+          );
+        })
+      );
+  }
+  createTask(data: Omit<ITask, 'id'>): Observable<ITask> {
+    return this.http
+      .post<ITask>(
+        `${this.api_url}/tasks`,
+        { data },
+        { headers: this.authService.getAuthHeaders() }
+      )
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching boards:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar colunas')
+          );
+        })
+      );
+  }
+  updateTask(data: ITask): Observable<ITask> {
+    return this.http
+      .put<ITask>(
+        `${this.api_url}/tasks/${data.id}`,
+        { data },
+        { headers: this.authService.getAuthHeaders() }
+      )
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching boards:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar colunas')
+          );
+        })
+      );
+  }
+  deleteTask(id: string): Observable<ITask> {
+    return this.http
+      .delete<ITask>(`${this.api_url}/tasks/${id}`, {
+        headers: this.authService.getAuthHeaders(),
+      })
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching boards:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar colunas')
+          );
+        })
+      );
+  }
+  moveTask(id: string, newColumnId: string): Observable<ITask> {
+    return this.http
+      .patch<ITask>(
+        `${this.api_url}/tasks/${id}/move`,
+        { newColumnId },
+        { headers: this.authService.getAuthHeaders() }
+      )
+      .pipe(
+        catchError((error) => {
+          console.error('Error fetching boards:', error);
+          return throwError(
+            () => new Error(error.error?.message || 'Erro ao buscar colunas')
+          );
+        })
+      );
+  }
+}
