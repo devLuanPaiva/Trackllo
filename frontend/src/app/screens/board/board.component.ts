291d162e666154f645a6e3d827ebc0d010d9873b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-06 17:49:19 -0300

    refactor:  render something while the data has not yet arrived (loading)

--- frontend/src/app/screens/board/board.component.ts
+++ frontend/src/app/screens/board/board.component.ts
@@ -4,14 +4,15 @@ import { Observable, switchMap } from 'rxjs';
 import { IBoard } from '../../models';
 import { BoardService } from '../../services/board.service';
 import { ColumnsComponent } from '../../components/board/columns/columns.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-board',
-  imports: [ColumnsComponent],
+  imports: [ColumnsComponent, CommonModule],
   templateUrl: './board.component.html',
 })
 export class BoardComponent implements OnInit {
-  board$!: Observable<IBoard | null>;
+  board$!: Observable<IBoard>;
   constructor(
     private readonly route: ActivatedRoute,
     private readonly boardService: BoardService
