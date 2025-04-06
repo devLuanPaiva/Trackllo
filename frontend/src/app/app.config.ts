e00f84e1aad777c9b195eef655702f5de6d406e6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-06 10:20:13 -0300

    chore: Add withFetch() in HttpClient configuration for SSR.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -2,8 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient } from '@angular/common/http';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,6 +14,6 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
     provideAnimationsAsync(),
-    provideHttpClient()
-  ]
+    provideHttpClient(withFetch()),
+  ],
 };
