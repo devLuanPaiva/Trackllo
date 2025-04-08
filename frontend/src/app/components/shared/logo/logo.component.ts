2a3339d66dbf1f00215863f30fba5db2f63a2b7b
Luan <devluanpaiva@gmail.com>
2025-04-08 08:47:19 -0300

    feat: create logo component

---
+++ frontend/src/app/components/shared/logo/logo.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTasks } from '@fortawesome/free-solid-svg-icons';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import {
+  trigger,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+@Component({
+  selector: 'app-logo',
+  imports: [FontAwesomeModule, MatTooltipModule],
+  templateUrl: './logo.component.html',
+  animations: [trigger('fadeInScale', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'scale(0,9)' }),
+      animate('400ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
+    ])
+  ])]
+})
+export class LogoComponent {
+  faTasks = faTasks;
+}
