354c0a831a0ab98407db62c2020a0b9e59ceb8de
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-08 20:29:50 -0300

    test: create mocks to all models

---
+++ frontend/src/app/mocks/index.ts
@@ -0,0 +1,4 @@
+export * from './mock-users';
+export * from './mock-tasks';
+export * from './mock-columns';
+export * from './mock-board';
---
+++ frontend/src/app/mocks/mock-board.ts
@@ -0,0 +1,11 @@
+import { IBoard } from '../models';
+import { mockColumns } from './mock-columns';
+
+export const mockBoards: IBoard[] = [
+  {
+    id: 'board-1',
+    title: 'Projeto Kanban',
+    userId: 'user-1',
+    columns: mockColumns,
+  },
+];
