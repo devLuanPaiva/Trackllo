c965b901523a0d1e38fa172bebfb2aa402efb0cd
Luan <devluanpaiva@gmail.com>
2025-04-11 07:36:53 -0300

    refactor: create local server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,31 +1,8 @@
 import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
-import { cors } from 'hono/cors'
-import userRoutes from './controllers/user.controller.js'
-import taskRoutes from './controllers/task.controller.js'
-import boardRoutes from './controllers/board.controller.js'
-import errorHandler from './utils/errorHandler.js'
-import columnRoutes from './controllers/column.controller.js'
+import createApp from './server/server.js'
 
-const app = new Hono().basePath('/api')
-app.use('*', cors())
-app.use('*', async (c, next) => {
-  console.log(`[${c.req.method}] ${c.req.url}`)
-  await next()
-})
+const app = createApp()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
-app.route('/users', userRoutes)
-app.route('/tasks', taskRoutes)
-app.route('/boards', boardRoutes)
-app.route('/columns', columnRoutes)
-app.onError(errorHandler)
-
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
+serve({ fetch: app.fetch, port: 3000 }, (info) => {
+  console.log(`✅ Server is running on http://localhost:${info.port}`)
 })
