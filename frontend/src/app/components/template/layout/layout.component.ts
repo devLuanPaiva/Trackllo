becf225f63454964a33f057eefdb6c03305b3422
Luan <devluanpaiva@gmail.com>
2025-04-16 08:36:08 -0300

    feat: add button to call logout function

--- frontend/src/app/components/template/layout/layout.component.ts
+++ frontend/src/app/components/template/layout/layout.component.ts
@@ -1,13 +1,38 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-
+import { Component, computed, inject, Input, signal } from '@angular/core';
+import { AuthenticationService } from '../../../services/authentication.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs';
 @Component({
   selector: 'app-layout',
-  imports: [CommonModule],
+  imports: [CommonModule, FontAwesomeModule],
   templateUrl: './layout.component.html',
 })
 export class LayoutComponent {
   @Input() className: string | undefined;
-  @Input() headerNone: boolean | undefined;
-  @Input() footerNone: boolean | undefined;
+
+  private readonly router = inject(Router);
+  private readonly authService = inject(AuthenticationService);
+  currentURL = signal(this.router.url)
+
+  constructor() {
+    this.router.events
+      .pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.currentURL.set(event.urlAfterRedirects)
+      })
+  }
+  showLayout = computed(() => {
+    const url = this.currentURL();
+    return url !== '/' && url !== '/autenticacao';
+  });
+  icon = {
+    logout: faArrowRightFromBracket
+  }
+
+  onLogout() {
+    this.authService.logout()
+  }
 }
