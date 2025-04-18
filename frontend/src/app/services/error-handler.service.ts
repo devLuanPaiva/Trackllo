33a595c6123d592e087a22b643d1e38a9204ce9a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-18 16:45:54 -0300

    feat: create error handler service

---
+++ frontend/src/app/services/error-handler.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ErrorHandlerService {
+  constructor() {}
+  handle(error: any, fallbackMessage: string = 'Erro inesperado') {
+    const message =
+      error?.error?.error?.errors?.[0]?.message ??
+      error?.error?.error?.message ??
+      error?.error?.message ??
+      fallbackMessage;
+    console.log(message);
+    return throwError(() => new Error(message));
+  }
+}
