ee325635de122474d2cef69a75009835311ba545
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-19 14:31:24 -0300

    feat: create fadeDialog animation

--- frontend/src/app/animations/index.ts
+++ frontend/src/app/animations/index.ts
@@ -76,3 +76,13 @@ export const latterAnimation = trigger('letterAnimation', [
     ]),
   ]),
 ]);
+
+export const fadeDialog = trigger('fadeDialog', [
+  transition(':enter', [
+    style({ opacity: 0, transform: 'scale(0.9)' }),
+    animate('200ms ease-out', style({ opacity: 1, transform: 'scale(1)' }))
+  ]),
+  transition(':leave', [
+    animate('150ms ease-in', style({ opacity: 0, transform: 'scale(0.95)' }))
+  ])
+])
