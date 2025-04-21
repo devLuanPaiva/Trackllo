4d9e66ab573403ad6e58238b10de81b5d0a85313
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-21 10:16:36 -0300

    test: add test to move inside same column

--- frontend/src/app/components/board/columns/columns.component.spec.ts
+++ frontend/src/app/components/board/columns/columns.component.spec.ts
@@ -4,7 +4,7 @@ import { ColumnsService } from '../../../services/columns.service';
 import { TasksService } from '../../../services/tasks.service';
 import { of } from 'rxjs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { mockBoards, mockColumns } from '../../../mocks';
+import { mockBoards, mockColumns, mockTasks } from '../../../mocks';
 
 describe('ColumnsComponent', () => {
   let component: ColumnsComponent;
@@ -31,10 +31,21 @@ describe('ColumnsComponent', () => {
     component.board = mockBoards[0];
   });
   it('should load board columns on init', () => {
-    mockColumnsService.getBoardColumns.and.returnValue(of(mockColumns))
+    mockColumnsService.getBoardColumns.and.returnValue(of(mockColumns));
     fixture.detectChanges();
-    expect(component.columnTodo.length).toBe(1)
-    expect(component.columnInProgress.length).toBe(1)
-    expect(component.columnDone.length).toBe(1)
-  })
+    expect(component.columnTodo.length).toBe(1);
+    expect(component.columnInProgress.length).toBe(1);
+    expect(component.columnDone.length).toBe(1);
+  });
+  it('should move task inside same column', () => {
+    component.columnTodo = [mockTasks[0], mockTasks[1]];
+    const event: any = {
+      previousContainer: { data: component.columnTodo },
+      container: { data: component.columnTodo },
+      previousIndex: 0,
+      currentIndex: 1,
+    };
+    component.onMoveColumnTask(event);
+    expect(component.columnTodo[1].id).toBe(mockTasks[0].id);
+  });
 });
