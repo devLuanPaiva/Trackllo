6d7114bb0c3fc4d36c9a01cc91e3285259116f01
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-21 09:31:32 -0300

    test: add test to display the custom message sent via MAT_DIALOG_DATA

---
+++ frontend/src/app/components/shared/dialog/dialog.component.spec.ts
@@ -0,0 +1,30 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { DialogComponent } from './dialog.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+
+describe('DialogComponent', () => {
+  let component: DialogComponent;
+  let fixture: ComponentFixture<DialogComponent>;
+  let dialogRef: jasmine.SpyObj<MatDialogRef<DialogComponent>>;
+
+  beforeEach(async () => {
+    dialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
+
+    await TestBed.configureTestingModule({
+      imports: [DialogComponent, BrowserAnimationsModule],
+      providers: [
+        { provide: MatDialogRef, useValue: dialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: { message: 'Custom message' } },
+      ],
+    }).compileComponents();
+    fixture = TestBed.createComponent(DialogComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+  it('should display the custom message sent via MAT_DIALOG_DATA', () => {
+    const messageEl = fixture.debugElement.query(By.css('h3')).nativeElement;
+    expect(messageEl.textContent).toContain('Custom message');
+  });
+});
