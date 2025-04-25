563224933ad40d7328c95b18cb10640f36f760f2
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-24 21:03:30 -0300

    refactor: update language component

--- frontend/src/app/components/shared/language/language.component.ts
+++ frontend/src/app/components/shared/language/language.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
 import { MatSelectModule, MatOption } from '@angular/material/select';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-language',
-  imports: [MatFormFieldModule, MatSelectModule, MatLabel, MatOption],
+  imports: [
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatLabel,
+    MatOption,
+    CommonModule,
+    TranslateModule,
+  ],
   templateUrl: './language.component.html',
 })
 export class LanguageComponent {
+  private readonly translate = inject(TranslateService);
+  selectedLang = this.translate.currentLang || 'pt';
+
   switchLanguage(lang: string) {
-    window.location.href = `/${lang}/`
+    this.translate.use(lang);
+    this.selectedLang = lang;
   }
 }
