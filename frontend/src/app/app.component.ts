8ee3b3abb0ef3ef0cebe87d8eea6599ed3a8301b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-25 21:13:08 -0300

    feat: load and set the language in the AppComponent

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LanguageComponent } from "./components/shared/language/language.component";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,8 @@ import { LanguageComponent } from "./components/shared/language/language.compone
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  constructor(private readonly translate: TranslateService) {
+    this.translate.setDefaultLang('pt');
+    this.translate.use('pt');
+  }
 }
