3f35053e5d99fad1228a29551a121eeb47074d8e
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-26 15:32:24 -0300

    style: improve language component style

--- frontend/src/app/components/shared/language/language.component.ts
+++ frontend/src/app/components/shared/language/language.component.ts
@@ -1,37 +1,50 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { MatFormFieldModule, MatLabel } from '@angular/material/form-field';
-import { MatSelectModule, MatOption } from '@angular/material/select';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faLanguage } from '@fortawesome/free-solid-svg-icons';
+
 @Component({
   selector: 'app-language',
+  standalone: true,
   imports: [
-    MatFormFieldModule,
-    MatSelectModule,
-    FormsModule,
-    MatLabel,
-    MatOption,
     CommonModule,
+    FormsModule,
     TranslateModule,
+    FontAwesomeModule,
   ],
   templateUrl: './language.component.html',
 })
 export class LanguageComponent {
   private readonly translate = inject(TranslateService);
   selectedLang = this.translate.currentLang || 'pt';
+  dropdownOpen = false;
+  icons = {
+    faLanguage,
+  };
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  closeDropdown() {
+    setTimeout(() => {
+      this.dropdownOpen = false;
+    }, 100); 
+  }
 
   switchLanguage(lang: string) {
     this.translate.use(lang).subscribe({
       next: () => {
         this.selectedLang = lang;
-
+        this.dropdownOpen = false;
       },
       error: (err) => {
         console.error('Failed to change language', err);
         this.translate.use('pt');
         this.selectedLang = 'pt';
-      }
+      },
     });
   }
 }
