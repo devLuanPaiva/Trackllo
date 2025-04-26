ffb2c07ee909efd3c7cfcbd5751bd1b2f0eac5ec
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-04-25 21:12:26 -0300

    fix: use the default loader @ngx-translate/http-loader

--- frontend/src/app/translate.config.ts
+++ frontend/src/app/translate.config.ts
@@ -1,10 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { importProvidersFrom } from '@angular/core';
-import { SafeTranslateLoader } from './custom-http-loader';
-
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 export function HttpLoaderFactory(http: HttpClient) {
-  return new SafeTranslateLoader(http, './assets/i18n/', '.json')
+  return new TranslateHttpLoader(http, '/assets/i18n/', '.json');
 }
 
 export const provideTranslation = () =>
