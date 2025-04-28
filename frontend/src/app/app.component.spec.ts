1debb8461b66089dd3ca449586705eb0a3ee3925
Luan <devluanpaiva@gmail.com>
2025-04-28 14:13:26 -0300

    test: add test to toggle dropdownOpen when toggleDropDown is called

---
+++ frontend/src/app/app.component.spec.ts
@@ -0,0 +1,41 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { AuthenticationService } from './services/authentication.service';
+import { LanguageComponent } from './components/shared/language/language.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { provideNoopAnimations } from '@angular/platform-browser/animations';
+
+
+describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let authServiceSpy: jasmine.SpyObj<AuthenticationService>;
+  let router: Router;
+  beforeEach(async () => {
+    const authSpy = jasmine.createSpyObj('AuthenticationService', ['logout']);
+
+    await TestBed.configureTestingModule({
+      imports: [CommonModule, LanguageComponent, FontAwesomeModule, AppComponent],
+      providers: [
+        { provide: AuthenticationService, useValue: authSpy },
+        provideNoopAnimations()
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    authServiceSpy = TestBed.inject(AuthenticationService) as jasmine.SpyObj<AuthenticationService>;
+    router = TestBed.inject(Router);
+
+    fixture.detectChanges();
+  });
+  it('should toggle dropdownOpen when toggleDropdown is called', () => {
+    expect(component.dropdownOpen).toBeFalse();
+    component.toggleDropdown();
+    expect(component.dropdownOpen).toBeTrue();
+    component.toggleDropdown();
+    expect(component.dropdownOpen).toBeFalse();
+  })
+})
