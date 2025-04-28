7c0574af2fb62de50016bda7ac3dd54017768003
Luan <devluanpaiva@gmail.com>
2025-04-28 14:01:20 -0300

    test: add test to change the response by removing BOM and parsing JSON

--- frontend/src/app/interceptors/encoding.interceptor.spec.ts
+++ frontend/src/app/interceptors/encoding.interceptor.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpRequest, HttpResponse } from '@angular/common/http';
 
 import { encodingInterceptor } from './encoding.interceptor';
+import { of } from 'rxjs';
 
 describe('encodingInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
+  const interceptor: HttpInterceptorFn = (req, next) =>
     TestBed.runInInjectionContext(() => encodingInterceptor(req, next));
 
   beforeEach(() => {
@@ -14,4 +15,22 @@ describe('encodingInterceptor', () => {
   it('should be created', () => {
     expect(interceptor).toBeTruthy();
   });
+  it('should change the response by removing BOM and parsing JSON', (done) => {
+    const mockReq = new HttpRequest('GET', 'assets/data.json');
+
+    const mockNext = (req: HttpRequest<any>) => {
+      expect(req.responseType).toBe('text');
+      const bodyWithBOM = '\uFEFF{"key":"value"}';
+      const response = new HttpResponse({ body: bodyWithBOM, status: 200 });
+      return of(response);
+    };
+
+    interceptor(mockReq, mockNext).subscribe((event) => {
+      expect(event instanceof HttpResponse).toBeTrue();
+      const response = event as HttpResponse<any>;
+      expect(response.body).toEqual({ key: 'value' });
+      done();
+    });
+  });
+
 });
