1b1fbb9dfd55a18fbbf92bac004d435e376b3836
Luan <devluanpaiva@gmail.com>
2025-04-28 14:24:48 -0300

    test: add test to update currentURL on navigation end

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { AuthenticationService } from './services/authentication.service';
 import { LanguageComponent } from './components/shared/language/language.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { provideNoopAnimations } from '@angular/platform-browser/animations';
+import { of } from 'rxjs';
 
 
 describe('AppComponent', () => {
@@ -15,11 +16,17 @@ describe('AppComponent', () => {
   let router: Router;
   beforeEach(async () => {
     const authSpy = jasmine.createSpyObj('AuthenticationService', ['logout']);
-
+    const fakeEvent = new NavigationEnd(0, '/old-url', '/new-url');
     await TestBed.configureTestingModule({
       imports: [CommonModule, LanguageComponent, FontAwesomeModule, AppComponent],
       providers: [
         { provide: AuthenticationService, useValue: authSpy },
+        {
+          provide: Router,
+          useValue: {
+            events: of(fakeEvent)
+          }
+        },
         provideNoopAnimations()
       ]
     }).compileComponents();
@@ -54,5 +61,17 @@ describe('AppComponent', () => {
 
     expect(component.dropdownOpen).toBeFalse();
   });
+  it('should update currentURL on navigation end', fakeAsync(() => {
+    const navEnd = new NavigationEnd(1, '/teste', '/teste');
+    (router.events as any) = of(navEnd);
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+
+    tick();
+    expect(component.currentURL()).toBe('/teste');
+
+  }))
 
 })
