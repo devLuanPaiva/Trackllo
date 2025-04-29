68b06176fd84d2964651b8a90034c1962611a4ce
Luan <devluanpaiva@gmail.com>
2025-04-29 14:24:21 -0300

    feat: set cors

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+
+  app.enableCors({
+    origin: [
+      'http://localhost:4200',
+      'https://trackllo.vercel.app',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization, X-Requested-With',
+  });
+
+  app.setGlobalPrefix('api');
+
   await app.listen(process.env.PORT ?? 3000);
+
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
