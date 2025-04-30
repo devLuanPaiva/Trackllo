b00c90d8175c313017bb276960877f4e6ffed43b
Luan <devluanpaiva@gmail.com>
2025-04-30 09:17:22 -0300

    feat: create column module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
 import { BoardModule } from './board/board.module';
+import { ColumnModule } from './column/column.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule, BoardModule],
+  imports: [DatabaseModule, UsersModule, BoardModule, ColumnModule],
   controllers: [AppController],
   providers: [AppService],
 })
