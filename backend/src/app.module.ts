92ccdf0142cb8f29ea508c58aaac5c6733ef3c52
Luan <devluanpaiva@gmail.com>
2025-04-30 08:35:40 -0300

    feat: create board module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UsersModule } from './users/users.module';
+import { BoardModule } from './board/board.module';
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [DatabaseModule, UsersModule, BoardModule],
   controllers: [AppController],
   providers: [AppService],
 })
