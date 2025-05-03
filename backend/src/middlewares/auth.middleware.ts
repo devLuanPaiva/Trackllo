737ed503f08aed5a9a872e3fda2d4ae45b3a151b
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-05-03 08:00:50 -0300

    fix: unsafe assignment of an error typed value.

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,24 +1,31 @@
-import { Injectable, NestMiddleware, UnauthorizedException } from '@nestjs/common';
-import { NextFunction } from 'express';
-import * as jwt from 'jsonwebtoken';
+import {
+	Injectable,
+	NestMiddleware,
+	UnauthorizedException,
+} from "@nestjs/common"
+import { NextFunction } from "express"
+import * as jwt from "jsonwebtoken"
 @Injectable()
 export class AuthMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    const authHeader = req.headers['authorization'];
-    const SECRET = process.env.JWT_SECRET ?? 'secrettoken';
-    if (!authHeader?.startsWith('Bearer ')) {
-      throw new UnauthorizedException('Unauthorized');
-    }
+	use(req: Request, res: Response, next: NextFunction) {
+		const authHeader =
+			typeof req.headers["authorization"] === "string"
+				? req.headers["authorization"]
+				: undefined
+		const SECRET = process.env.JWT_SECRET ?? "secrettoken"
+		if (!authHeader?.startsWith("Bearer ")) {
+			throw new UnauthorizedException("Unauthorized")
+		}
 
-    const token = authHeader.split(' ')[1];
+		const token = authHeader.split(" ")[1]
 
-    try {
-      const decoded = jwt.verify(token, SECRET) as { id: string };
-      req['userId'] = decoded.id;
-      next();
-    } catch (err) {
-      console.log(err);
-      throw new UnauthorizedException('Invalid token');
-    }
-  }
+		try {
+			const decoded = jwt.verify(token, SECRET) as { id: string }
+			req["userId"] = decoded.id
+			next()
+		} catch (err) {
+			console.log(err)
+			throw new UnauthorizedException("Invalid token")
+		}
+	}
 }
