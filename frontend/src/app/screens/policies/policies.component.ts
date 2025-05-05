8bf4d01ac08a5359c24d05752764842d8c75e506
Luan <devluanpaiva@gmail.com>
2025-05-05 13:44:58 -0300

    feat: translate messages

--- frontend/src/app/screens/policies/policies.component.ts
+++ frontend/src/app/screens/policies/policies.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-policies',
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './policies.component.html',
 })
 export class PoliciesComponent {
