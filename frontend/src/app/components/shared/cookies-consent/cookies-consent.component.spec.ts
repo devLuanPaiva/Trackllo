b61e93f44f7ef4ffb5e4e2031965efbe92e95887
Luan <devluanpaiva@gmail.com>
2025-05-06 09:44:26 -0300

    test: add the animation and translation modules to the test environment

--- frontend/src/app/components/shared/cookies-consent/cookies-consent.component.spec.ts
+++ frontend/src/app/components/shared/cookies-consent/cookies-consent.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { CookiesConsentComponent } from './cookies-consent.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateModule, TranslateService, TranslateStore } from '@ngx-translate/core';
 
 describe('CookiesConsentComponent', () => {
   let component: CookiesConsentComponent;
@@ -8,9 +9,13 @@ describe('CookiesConsentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CookiesConsentComponent]
+      imports: [CookiesConsentComponent, TranslateModule.forRoot(), NoopAnimationsModule],
+      providers: [
+        TranslateService,
+        TranslateStore
+      ]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(CookiesConsentComponent);
     component = fixture.componentInstance;
